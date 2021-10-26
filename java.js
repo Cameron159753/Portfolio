63799c49a1e81df57312196e6a257fa326e2a662
Cameron Bowers <cameronbowers22@gmail.com>
2021-10-26 16:00:32 +0200

    needs to be fixed

--- java.js
+++ java.js
@@ -1,16 +1,37 @@
-var dropdown = document.getElementsByClassName("dropdown-btn");
-var i;
+// var dropdown = document.getElementsByClassName("dropdown-btn");
+// var i;
 
-for (i = 0; i < dropdown.length; i++) {
-  dropdown[i].addEventListener("click", function() {
-  this.classList.toggle("active");
-  var dropdownContent = this.nextElementSibling;
-  if (dropdownContent.style.display === "block") {
-  dropdownContent.style.display = "none";
-  } else {
-  dropdownContent.style.display = "block";
-  }
+// for (i = 0; i < dropdown.length; i++) {
+//   dropdown[i].addEventListener("click", function() {
+//   this.classList.toggle("active");
+//   var dropdownContent = this.nextElementSibling;
+//   if (dropdownContent.style.display === "block") {
+//   dropdownContent.style.display = "none";
+//   } else {
+//   dropdownContent.style.display = "block";
+//   }
+//   });
+// }
+
+let sidebar = document.querySelector(".sidebar");
+  let closeBtn = document.querySelector("#btn");
+  let searchBtn = document.querySelector(".bx-search");
+
+  closeBtn.addEventListener("click", ()=>{
+    sidebar.classList.toggle("open");
+    menuBtnChange();//calling the function(optional)
   });
-}
 
+  searchBtn.addEventListener("click", ()=>{ // Sidebar open when you click on the search iocn
+    sidebar.classList.toggle("open");
+    menuBtnChange(); //calling the function(optional)
+  });
 
+  // following are the code to change sidebar button(optional)
+  function menuBtnChange() {
+   if(sidebar.classList.contains("open")){
+     closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");//replacing the iocns class
+   }else {
+     closeBtn.classList.replace("bx-menu-alt-right","bx-menu");//replacing the iocns class
+   }
+  }
